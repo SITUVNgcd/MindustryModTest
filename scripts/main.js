edf569aae0f033ba1a2552cfb79738f028c2eae3
SITUVNgcd <44901211+SITUVNgcd@users.noreply.github.com>
2023-02-26 18:01:33 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -96,7 +96,7 @@ function showCredits(){
 function line(s, r){
   let tbl = new Table();
   tbl.add((r ? "< " : "[lightgray]> ") + s.replace("[", "[[") + "[]").left().wrap().padLeft(6).growX();
-  tbl.button(new TextureRegionDrawable(Icon.copy), 24, ()=>{Core.app.setClipboardText(s);}); //.right().padLeft(6).padRight(6);
+  tbl.button(new TextureRegionDrawable(Icon.copy), 24, ()=>{Core.app.setClipboardText(s);}).top().padLeft(6).padRight(6);
   return tbl;
 }
 
