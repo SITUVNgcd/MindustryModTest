8b3bb34d77609ca39b806c28b540d0e873227f29
SITUVNgcd <44901211+SITUVNgcd@users.noreply.github.com>
2023-02-28 20:46:42 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -52,10 +52,10 @@ function addTable(table){
       }
     }).padLeft(6).get();
   });
-  let xxx = true;
-  let col = table.collapser(tbl, ()=>{xxx=!xxx; return xxx;}).padLeft(6).width(400).height(400).get();
+  let xxx = false;
+  let col = table.collapser(tbl, ()=>{return xxx;}).padLeft(6).width(400).height(400).get();
   //table.add(tbl);
-  table.button("XXX", ()=>col.toggle()).padLeft(6).width(40).height(40);
+  table.button("XXX", ()=>{xxx=!xxx;}).padLeft(6).width(40).height(40);
   table.visibility = () => {
     if(!Vars.ui.hudfrag.shown || Vars.ui.minimapfrag.shown()) return false;
     if(!Vars.mobile) return true;
