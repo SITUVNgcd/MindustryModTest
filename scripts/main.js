741e4f4547482ff92f36f17e80e1859edd7986d2
SITUVNgcd <44901211+SITUVNgcd@users.noreply.github.com>
2023-03-01 21:41:13 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -166,7 +166,7 @@ function runScript(s){
   let r;
   let script = Vars.mods.getScripts();
   //r = script.context.evaluateString(script.scope, s, "situvn-console.js", 1, null);
-  r = script.runConsole(s):
+  //r = script.runConsole(s):
   /*
   if(r == undefined){
     r = "undefined";
