26376b8acaed7bc960f324e7e5c4916ca214220e
SITUVNgcd <44901211+SITUVNgcd@users.noreply.github.com>
2023-03-06 00:19:02 +0700

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -76,7 +76,7 @@ function addTable(table){
       try{
       if(!con){
         let hg = Vars.ui.hudGroup;
-        con = require("test-mod/console");
+        con = require("situvngcd.test.mod/console");
         con.tbl.top().right();
         con.tbl.setWidth(400);
         con.tbl.setHeight(600);
