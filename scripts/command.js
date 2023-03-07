713014a86578672c98bb57cb5bbfb587bd3f8630
SITUVNgcd <44901211+SITUVNgcd@users.noreply.github.com>
2023-03-08 00:13:49 +0700

    Add files via upload

--- scripts/command.js
+++ scripts/command.js
@@ -1,7 +1,7 @@
 Events.on(ClientLoadEvent, () => {
   try{
     let cmd = findCommandButton();
-    let par = cmd.parent;
+    let par = findParent(cmd);
     let cmdW = cmd.width;
     par.clear();
     par.add(cmd).width(cmdW);
@@ -41,5 +41,21 @@ function findCommandButton(){
     let [bd, cf] = [Core.bundle, "get(java.lang.String,java.lang.String)"];
     let cct = bd[cf]("command", "Command");
     return e instanceof TextButton && e.getText() == cct;
+  });
+}
+
+function findParent(c){
+  if(!c){
+    return null;
+  }
+  let hg = Vars.ui.hudGroup;
+  return hg["find(arc.func.Boolf)"](e=>{
+    let cc = null;
+    e.forEach(f=>{
+      if(f == c){
+        cc = f;
+      }
+    });
+    return c == cc;
   });
 }
