681879f0919579f12e6ad5f07bc7a1a6bbfc7994
SITUVNgcd <44901211+SITUVNgcd@users.noreply.github.com>
2023-03-20 19:39:21 +0700

    Add files via upload

--- scripts/misc.js
+++ scripts/misc.js
@@ -1,14 +1,18 @@
 Events.on(ClientLoadEvent, ()=>{
-  let settings = Core.settings;
-  
-  let [hg, fe] = [Vars.ui.hudGroup, "find(arc.func.Boolf)"];
-  let cid = hg[fe](e=>{
-    return e instanceof Collapser && e.forEach(f=>f instanceof CoreItemsDisplay) != null;
-  });
-  let boss = hg.find("boss");
-  
-  cid.setCollapsed(()=>!Vars.ui.hudfrag.shown || !settings.getBool("svn-force-show-item-info"));
-  boss.visibility=()=>Vars.ui.hudfrag.shown && settings.getBool("svn-force-show-boss-info");
-  Vars.renderer.minZoom=0.3;
-  Vars.renderer.maxZoom=15
+  try{
+    let settings = Core.settings;
+    
+    let [hg, fe] = [Vars.ui.hudGroup, "find(arc.func.Boolf)"];
+    let cid = hg[fe](e=>{
+      return e instanceof Collapser && e[fe](f=>f instanceof CoreItemsDisplay) != null;
+    });
+    let boss = hg.find("boss");
+    
+    cid.setCollapsed(()=>!Vars.ui.hudfrag.shown || !settings.getBool("svn-force-show-item-info"));
+    boss.visibility=()=>Vars.ui.hudfrag.shown && settings.getBool("svn-force-show-boss-info");
+    Vars.renderer.minZoom=0.3;
+    Vars.renderer.maxZoom=15
+  }catch(e){
+    Log.info(e);
+  }
 });
