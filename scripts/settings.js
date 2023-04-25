e6841fcca386982edf694233473b8cc936c58b23
SITUVNgcd <44901211+SITUVNgcd@users.noreply.github.com>
2023-04-25 23:37:10 +0700

    Add files via upload

--- scripts/settings.js
+++ scripts/settings.js
@@ -21,37 +21,38 @@ try{
     });
     return res;
   }
-  
-  try{
-    const settings = Vars.ui.settings;
-    const bd = Core.bundle;
-    settings.addCategory("@setting.svn-settings.name", st=>{
-      if(Vars.mobile){
-        st.pref(section("svn-section-console"));
-        st.checkPref("svn-system-log", false);
-        st.checkPref("svn-console", false);
-        st.checkPref("svn-console-use-runConsole", false);
-        st.pref(section("svn-section-misc"));
-        st.checkPref("svn-time-control", false);
-        st.checkPref("svn-force-show-item-info", false);
-        st.checkPref("svn-force-show-boss-info", false);
-        st.checkPref("svn-tile-block-unit-info", false);
-        st.checkPref("svn-chat-command-helper", false);
-        st.checkPref("svn-crawler-arena-helper", false);
-        st.sliderPref("svn-min-zoom", Vars.renderer.minZoom, 1, 100, 1, v=>{
-          v = v / 10;
-          Vars.renderer.minZoom = v;
-          return v;
-        });
-        st.sliderPref("svn-max-zoom", Vars.renderer.maxZoom, 10, 100, 1, v=>{
-          Vars.renderer.maxZoom = v;
-          return v;
-        });
-      }
-    });
-  }catch(e){
-    Log.err("settings: " + e);
-  }
+  Events.on(ClientLoadEvent, () => {
+    try{
+      const settings = Vars.ui.settings;
+      const bd = Core.bundle;
+      settings.addCategory("@setting.svn-settings.name", st=>{
+        if(Vars.mobile){
+          st.pref(section("svn-section-console"));
+          st.checkPref("svn-system-log", false);
+          st.checkPref("svn-console", false);
+          st.checkPref("svn-console-use-runConsole", false);
+          st.pref(section("svn-section-misc"));
+          st.checkPref("svn-time-control", false);
+          st.checkPref("svn-force-show-item-info", false);
+          st.checkPref("svn-force-show-boss-info", false);
+          st.checkPref("svn-tile-block-unit-info", false);
+          st.checkPref("svn-chat-command-helper", false);
+          st.checkPref("svn-crawler-arena-helper", false);
+          st.sliderPref("svn-min-zoom", Vars.renderer.minZoom, 1, 100, 1, v=>{
+            v = v / 10;
+            Vars.renderer.minZoom = v;
+            return v;
+          });
+          st.sliderPref("svn-max-zoom", Vars.renderer.maxZoom, 10, 100, 1, v=>{
+            Vars.renderer.maxZoom = v;
+            return v;
+          });
+        }
+      });
+    }catch(e){
+      Log.err("settings: " + e);
+    }
+  });
 }catch(e){
   Log.err("settings: " + e);
 }
