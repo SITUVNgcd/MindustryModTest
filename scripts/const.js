e821e7a586bb994217658f807dd2febdd68d4f99
SITUVNgcd <44901211+SITUVNgcd@users.noreply.github.com>
2023-05-01 00:24:08 +0700

    Add files via upload

--- scripts/const.js
+++ scripts/const.js
@@ -7,6 +7,8 @@ try{
     let eu = "";
     let ei = "";
     let el = "";
+    let es = "";
+    let et = "";
     let emoji = function(seq){
       let r = "", ii;
       for(let i = 0; i < seq.size; ++i){
@@ -23,12 +25,18 @@ try{
     eu = emoji(c.units());
     ei = emoji(c.items());
     el = emoji(c.liquids());
+    es = emoji(c.statusEffects());
+    for(let i of Team.all){
+      et += i.emoji;
+    }
     global.svn.const.emojiIcons = ee;
     global.svn.const.emojiBlocks = eb;
     global.svn.const.emojiUnits = eu;
     global.svn.const.emojiItems = ei;
     global.svn.const.emojiLiquids = el;
-    global.svn.const.emojiAll = ee + eb + eu + ei + el;
+    global.svn.const.emojiStatusEffects = es;
+    global.svn.const.emojiTeams = et;
+    global.svn.const.emojiAll = ee + eb + eu + ei + el + es + et;
   });
 }catch(e){
   Log.err("const: " + e);
