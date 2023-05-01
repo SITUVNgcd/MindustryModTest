acc924692e3b9c7e423b147c113d653fad6b1308
SITUVNgcd <44901211+SITUVNgcd@users.noreply.github.com>
2023-05-01 21:27:56 +0700

    Add files via upload

--- scripts/misc.js
+++ scripts/misc.js
@@ -24,7 +24,7 @@ Events.on(ClientLoadEvent, ()=>{
     let tct = tc.run();
     tct.setPosition(0, 120);
     const tctv = tct.visibility;
-    tct.visibility = ()=> hf.shown && Core.settings.getBool("svn-time-control") || tctv();
+    tct.visibility = ()=> hf.shown && Core.settings.getBool("svn-time-control") || tctv.get();
     hg.addChild(tct);
   }catch(e){
     Log.err("misc: " + e);
