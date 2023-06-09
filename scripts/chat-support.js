7508fcf61b175fbe478aef151fe25155104a88b5
SITUVNgcd <44901211+SITUVNgcd@users.noreply.github.com>
2023-06-09 23:00:15 +0700

    Add files via upload

--- scripts/chat-support.js
+++ scripts/chat-support.js
@@ -139,7 +139,7 @@ try{
         arr = global.svn.players;
         let it, pls = arr.cur;
         const up = ()=>{
-          Log.info("UPDATE PLS!");
+          Log.info("UPDATE PLS! " + pls.size);
           bot.clearChildren();
           for(i = 0; i < pls.size; ++i){
             it = pls.get(i);
