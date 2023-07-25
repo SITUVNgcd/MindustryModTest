abfe8394cdb29bd328c1d8ed46d4b36bfa62f4fd
SITUVNgcd <44901211+SITUVNgcd@users.noreply.github.com>
2023-07-26 00:00:38 +0700

    Add files via upload

--- scripts/layout.js
+++ scripts/layout.js
@@ -58,6 +58,39 @@ try{
       layout.bottomRight = function(b){
         return take(8, b);
       }
+      
+      layout.floatDlg = function(b, mw, mh, pa){
+        const hg = Vars.ui.hudGroup;
+        const p = new Table();
+        p.setFillParent(true);
+        const tbl = new Table();
+        tbl.background(b && b instanceof Drawable ? b : Tex.buttonTrans);
+        tbl.toFront();
+        hg.addChild(p);
+        if(typeof mw !== "number" || mw < 0){
+          mw = 200;
+        }
+        if(typeof mh !== "number" || mh < 0){
+          mh = 200;
+        }
+        if(typeof pa !== "number" || pa < 0){
+          pa = 200;
+        }
+        p.add(tbl).minWidth(mw).minHeight(mh).pad(pa).margin(10);
+        const top = new Table(), mid = new Table(), bot = new Table();
+        tbl.touchable = Touchable.enabled;
+        tbl.add(top).growX().top().minHeight(60);
+        tbl.row();
+        tbl.image(Tex.whiteui, Pal.accent).growX().height(3).pad(6);
+        tbl.row();
+        tbl.add(mid).grow();
+        tbl.row();
+        tbl.image(Tex.whiteui, Pal.accent).growX().height(3).pad(6);
+        tbl.row();
+        tbl.add(bot).growX().bottom().minHeight(60);
+        return {tbl: tbl, top: top, mid: mid, bot: bot}
+      }
+      
       global.svn.layout = layout;
     }catch(e){
       Log.err(module.id + ": " + e);
