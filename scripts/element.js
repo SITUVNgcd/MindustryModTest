9c568c7b7b5a0f9f04c2afceddf6e0b064f72431
SITUVNgcd <44901211+SITUVNgcd@users.noreply.github.com>
2023-08-08 19:43:06 +0700

    Add files via upload

--- scripts/element.js
+++ scripts/element.js
@@ -44,6 +44,41 @@ try{
   }
   
   global.svn.ele.expand = expand;
+  
+  const teamChooser = function(fil, clk, upd, ipr){
+    if(typeof ipr !== "number" || ipr < 1){
+      ipr = 8;
+    }
+    const cont = new Table();
+    const butG = new ButtonGroup();
+    const ts = Team.all;
+    if(typeof fil === "function"){
+      ts = ts.filter(fil);
+    }
+    let b, i, bc;
+    for(i = 0; i < ts.length; ++i){
+      let team = ts[i];
+      b = new ImageButton(Tex.whiteui, Styles.clearNoneTogglei);
+      b.margin(4);
+      b.getImageCell().grow();
+      b.getStyle().imageUpColor = team.color;
+      if(typeof clk === "function"){
+        b.clicked(()=>{
+          clk(team, i);
+        });
+      }
+      bc = cont.add(b).size(50).group(butG);
+      if(typeof upd === "function"){
+        bc.checked(()=>{
+          return upd(team, i);
+        });
+      }
+      if(i % ipr == (ipr - 1)){
+        cont.row();
+      }
+    }
+  }
+  global.svn.ele.teamChooser = teamChooser;
 }catch(e){
   Log.err(module.id + ": " + e);
 }
