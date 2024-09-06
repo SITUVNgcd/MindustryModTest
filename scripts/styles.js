5077658f2155bd9af49b0b8aa9e7fdb5a5d0a113
SITUVNgcd <44901211+SITUVNgcd@users.noreply.github.com>
2024-09-06 14:59:44 +0700

    Flat styles

--- scripts/styles.js
+++ scripts/styles.js
@@ -4,13 +4,18 @@ try{
     const flati = new ImageButton.ImageButtonStyle(Styles.flati);
     Object.assign(flati, {
       up: Styles.black3,
-      down: Styles.black5
+      down: Styles.black5,
+      imageUpColor: Color.white,
+      imageDisabledColor: Color.darkGray,
+      imageCheckedColor: Pal.accent
     });
     global.svn.styles.flati = flati;
     const flatt = new TextButton.TextButtonStyle(Styles.flatt);
     Object.assign(flatt, {
       up: Styles.black3,
-      down: Styles.black5
+      down: Styles.black5,
+      disabledFontColor: Color.darkGray
+      checkedFontColor: Pal.accent;
     });
     global.svn.styles.flatt = flatt;
   });
