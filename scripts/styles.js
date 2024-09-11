1a0ab168923b5bc7cc9fca456f5db570d800229e
SITUVNgcd <44901211+SITUVNgcd@users.noreply.github.com>
2024-09-11 23:38:07 +0700

    styles catch

--- scripts/styles.js
+++ scripts/styles.js
@@ -1,25 +1,29 @@
 try{
   global.svn.styles = {};
   global.svn.evt.load(()=>{
-    const flati = new ImageButton.ImageButtonStyle(Styles.flati);
-    Object.assign(flati, {
-      up: Styles.black3,
-      down: Styles.black5,
-      imageUpColor: Color.white,
-      imageDisabledColor: Color.darkGray,
-      imageCheckedColor: Color.white
-    });
-    global.svn.styles.flati = flati;
-    const flatt = new TextButton.TextButtonStyle(Styles.flatt);
-    Object.assign(flatt, {
-      up: Styles.black3,
-      down: Styles.black5,
-      disabledFontColor: Color.darkGray,
-      checkedFontColor: Color.white
-    });
-    global.svn.styles.flatt = flatt;
-    const flattogglet = new TextButton.TextButtonStyle(Styles.flattogglet);
-    global.svn.styles.flattogglet = flattogglet;
+    try{
+      const flati = new ImageButton.ImageButtonStyle(Styles.flati);
+      Object.assign(flati, {
+        up: Styles.black3,
+        down: Styles.black5,
+        imageUpColor: Color.white,
+        imageDisabledColor: Color.darkGray,
+        imageCheckedColor: Color.white
+      });
+      global.svn.styles.flati = flati;
+      const flatt = new TextButton.TextButtonStyle(Styles.flatt);
+      Object.assign(flatt, {
+        up: Styles.black3,
+        down: Styles.black5,
+        disabledFontColor: Color.darkGray,
+        checkedFontColor: Color.white
+      });
+      global.svn.styles.flatt = flatt;
+      const flattogglet = new TextButton.TextButtonStyle(Styles.flattogglet);
+      global.svn.styles.flattogglet = flattogglet;
+    }catch(e){
+      Log.err(e);
+    }
   });
 }catch(e){
   Log.err(module.id + ": " + e);
